4a9740da8377100b4d82dedb11ae1aca95edd7de
andy <rui@glasscave.co.uk>
2017-12-01 19:18:09 +0000

    added block_availability endpoint

--- lib/booking_api/client.rb
+++ lib/booking_api/client.rb
@@ -20,7 +20,6 @@ module BookingApi
       then
         raise ArgumentError.new('You must supply one of the following parameters: hotel_ids: [], city_ids: [], country_ids: [], region_ids: [], district_ids: [], chain_ids: []')
       end
-
       default_parameters = {}
       default_parameters[:hotel_ids] = hotel_ids.join(',') if hotel_ids.any?
       default_parameters[:city_ids] = city_ids.join(',') if city_ids.any?
@@ -29,7 +28,6 @@ module BookingApi
       default_parameters[:district_ids] = district_ids.join(',') if district_ids.any?
       default_parameters[:chain_ids] = chain_ids.join(',') if chain_ids.any?
       default_parameters[:language] = 'en'
-      #request_parameters[:extras] = 'hotel_info'
       http_service.request_get('/json/hotels', default_parameters.merge(request_parameters))
     end
 
@@ -44,13 +42,33 @@ module BookingApi
     end
 
     # Implementation of the changed hotels end point
-    def changedHotels(last_change:, request_parameters: {})
+    def changed_hotels(last_change:, request_parameters: {})
       default_parameters = {}
-      default_parameters[:last_change] = last_change.strftime('%Y-%m-%d %H:%M:%S')
+      default_parameters[:last_change] = api_timestamp(last_change)
       http_service.request_get('/json/changedHotels', default_parameters.merge(request_parameters))
     end
 
+    # Implementation of block availability end point
+    # Confusing but this is used to find out rooms available at specific hotel on dates provided
+    def block_availability(hotel_id:, checkin_date:, checkout_date:, request_parameters: {})
+      default_parameters = {}
+      default_parameters[:hotel_ids] = hotel_id
+      default_parameters[:checkin] = api_date(checkin_date)
+      default_parameters[:checkout] = api_date(checkout_date)
+      http_service.request_get('/json/blockAvailability', default_parameters.merge(request_parameters))
+    end
+
     private
+
+    # Return timestamp in the correct format
+    def api_timestamp(date)
+      date.strftime('%Y-%m-%d %H:%M:%S')
+    end
+
+    # Return date in the correct format
+    def api_date(date)
+      date.strftime('%Y-%m-%d')
+    end
 
   end
 end
